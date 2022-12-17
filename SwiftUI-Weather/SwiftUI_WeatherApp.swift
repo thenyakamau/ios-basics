2ecad5fbb9e4bff35edb892d75f7d4f56754161b
Thenya Kamau <thenyakamau@gmail.com>
2022-12-17 04:32:39 +0300

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Thenya Kamau on 17/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
